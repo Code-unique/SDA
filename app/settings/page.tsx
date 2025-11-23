002a3a150b42a77d967cc3491e665e3a426bc440
code-unique <uniqueadhikari2534@gmail.com>
2025-11-23 12:55:45 +0545

    added settings page

---
+++ app/settings/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='text-6xl bg-black text-white'>
+        settings page
+        coming soon ........
+    </div>
+  )
+}
+
+export default page
